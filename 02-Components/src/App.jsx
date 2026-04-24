1b2654067ce1bf2f9d857ed2ef61e8f41c08d5e7
Eshwar <eshwarhs170@gmail.com>
2026-04-24 17:29:56 +0530

    learned some components of react

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Navbar />
+      <div className='card'>
+        <h1>HELLO </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ipsum dolorum dignissimos quo ad sint, cum reprehenderit maiores voluptates cumque nemo corrupti, quidem harum quibusdam. Optio similique quod aliquam hic!</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 02-Components/src/components/card.jsx
@@ -0,0 +1,4 @@
+function Card(){
+    return 'hi,i am card';
+}
+export default Card;
