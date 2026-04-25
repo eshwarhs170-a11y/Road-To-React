cc32b563c1e709134a122e0d36a2b78e699f4f08
Eshwar <eshwarhs170@gmail.com>
2026-04-25 23:17:14 +0530

    did simple UI card css design

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Card from './components/Card'
+import  User  from './components/User'
+
+const App = () => {
+  
+  return (
+    <div className='parent'>
+     {/* <Card />
+     <User name='Ramu'/>
+     <User name='Shamu'/>
+     <User name='Bhamu'/>
+     <User name={arr[0]}/>
+     <User name={arr[1]}/> */}
+     
+    </div>
+  )
+}
+
+export default App
