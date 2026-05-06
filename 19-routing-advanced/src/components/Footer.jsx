f601d10b0a6995ceead40a58261e482f06ad0eb0
Eshwar <eshwarhs170@gmail.com>
2026-05-06 23:23:37 +0530

    understood different varieties of routing with a example

---
+++ 19-routing-advanced/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex absolute items-center justify-center bottom-0 w-full bg-cyan-900 py-4 '>
+      This is Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ 19-routing-advanced/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const About = () => {
+
+  const navigate= useNavigate()
+
+ 
+  return (
+    <div>
+     
+      <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default About
